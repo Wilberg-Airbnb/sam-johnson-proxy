703496b12e32a739b1c713a0306caeb8fbbd3ab3
Sam Johnson <samj@iconcollective.com>
2020-07-06 16:49:08 -0700

    added routes to expess server to get data for listing from service

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const axios = require('axios');
 
 app.use('/:listingId', express.static(__dirname + '../../public'));
 app.use(express.json());
 
-// app.get('/:listingId', (req, res) => {
-//   console.log('GET')
-//   console.log('path: ', req.path)
-//   const listingPage = path.join(__dirname, '../public/index.html')
-//   res.sendFile(listingPage);
-// })
-
+app.get('/api/reservation/:listingId', (req, res) => {
+  console.log('GET')
+  console.log('path: ', req.params.listingId)
+  axios.get(`http://localhost:5000/api/reservation/${req.params.listingId}`)
+    .then(data => {
+      console.log('data from reservation: ', data);
+      res.send(data.data)
+    })
+    .catch(err => {
+      console.log('failed to get reservation data for listing', err)
+    })
+})
 
 module.exports.app = app;
