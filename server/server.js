f0efe535c6ae05e52152ad1c10c544e760c43185
Sam Johnson <samj@iconcollective.com>
2020-07-07 17:36:47 -0700

    complete adding description, location, reviews and reservation bundles together on page

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const { app } = require('./index.js');
 
-app.listen(8080, () => {
-  console.log('server listening on port 8080!');
+app.listen(8888, () => {
+  console.log('server listening on port 8888!');
 })
