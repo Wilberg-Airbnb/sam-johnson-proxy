b248fae24b5532e855f23bee6f085fd046dff66b
Sam Johnson <samj@iconcollective.com>
2020-07-25 12:13:24 -0700

    updated services for 7/25 and removed redirect routes

--- server/server.js
+++ server/server.js
@@ -4,28 +4,28 @@ app.listen(8888, () => {
   console.log('server listening on port 8888!');
 })
 
-app.get('/api/reservation/:listingId', (req, res) => {
-  res.redirect(`http://3.19.16.18/api/reservation/${req.params.listingId}`)
-})
+// app.get('/api/reservation/:listingId', (req, res) => {
+//   res.redirect(`http://3.19.16.18/api/reservation/${req.params.listingId}`)
+// })
 
-app.get('/api/location/:listingId', (req, res) => {
-  res.redirect(`http://ec2-3-12-169-208.us-east-2.compute.amazonaws.com:2001/api/location/${req.params.listingId}`)
-})
+// app.get('/api/location/:listingId', (req, res) => {
+//   res.redirect(`http://ec2-3-12-169-208.us-east-2.compute.amazonaws.com:2001/api/location/${req.params.listingId}`)
+// })
 
-app.get('/:listingId/airbrb_home.png', (req, res) => {
-  res.redirect(`http://ec2-3-12-169-208.us-east-2.compute.amazonaws.com:2001/${req.params.listingId}/airbrb_home.png`)
-})
+// app.get('/:listingId/airbrb_home.png', (req, res) => {
+//   res.redirect(`http://ec2-3-12-169-208.us-east-2.compute.amazonaws.com:2001/${req.params.listingId}/airbrb_home.png`)
+// })
 
-app.get('/api/description/:listingId', (req, res) => {
-  res.redirect(`http://ec2-3-15-150-168.us-east-2.compute.amazonaws.com:4000/api/description/${req.params.listingId}`)
-})
+// app.get('/api/description/:listingId', (req, res) => {
+//   res.redirect(`http://ec2-3-15-150-168.us-east-2.compute.amazonaws.com:4000/api/description/${req.params.listingId}`)
+// })
 
-app.get('/api/suggestions/:listingId', (req, res) => {
-  res.redirect(`http://ec2-3-129-14-177.us-east-2.compute.amazonaws.com:8081/api/suggestions/${req.params.listingId}`)
-})
+// app.get('/api/suggestions/:listingId', (req, res) => {
+//   res.redirect(`http://ec2-3-129-14-177.us-east-2.compute.amazonaws.com:8081/api/suggestions/${req.params.listingId}`)
+// })
 
-app.get('/api/reviews/:listingId', (req, res) => {
-  console.log('reviews')
-  if (req.query.type === 'review') {res.redirect(`http://ec2-3-129-14-177.us-east-2.compute.amazonaws.com:8080/api/reviews/${req.params.listingId}?type=review`)}
-  res.redirect(`http://ec2-3-129-14-177.us-east-2.compute.amazonaws.com:8080/api/reviews/${req.params.listingId}`)
-})
+// app.get('/api/reviews/:listingId', (req, res) => {
+//   console.log('reviews')
+//   if (req.query.type === 'review') {res.redirect(`http://ec2-3-129-14-177.us-east-2.compute.amazonaws.com:8080/api/reviews/${req.params.listingId}?type=review`)}
+//   res.redirect(`http://ec2-3-129-14-177.us-east-2.compute.amazonaws.com:8080/api/reviews/${req.params.listingId}`)
+// })
